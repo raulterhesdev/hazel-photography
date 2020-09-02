08bed4d7511268a14f87c2a308fecf1b950acbbd
Raul Terhes <raulterhesdevelopment@gmail.com>
2020-09-02 17:18:48 +0300

    initial portofolio setup + js functionality for displaying/filtering and opening images in modal

---
+++ js/data.js
@@ -0,0 +1,102 @@
+// filter categories
+const categories = [
+  {
+    name: 'All',
+    filter: 'all',
+  },
+  {
+    name: 'Landscape',
+    filter: 'landscape',
+  },
+  {
+    name: 'Portrait',
+    filter: 'portrait',
+  },
+  {
+    name: 'Wedding',
+    filter: 'wedding',
+  },
+  {
+    name: 'Drone',
+    filter: 'drone',
+  },
+  {
+    name: 'Baby',
+    filter: 'baby',
+  },
+];
+
+const images = [
+  {
+    id: '1',
+    url: 'wave.jpg',
+    category: 'landscape',
+    title: 'Wave',
+  },
+  {
+    id: '2',
+    url: 'aurora.jpg',
+    category: 'landscape',
+    title: 'Aurora',
+  },
+  {
+    id: '3',
+    url: 'wedding2.jpg',
+    category: 'wedding',
+    title: 'Wedding 2',
+  },
+  {
+    id: '4',
+    url: 'seadrone.jpg',
+    category: 'drone',
+    title: 'Beach and Sea from drone',
+  },
+  {
+    id: '5',
+    url: 'mountain.jpg',
+    category: 'landscape',
+    title: 'Mountain',
+  },
+  {
+    id: '6',
+    url: 'roaddrone.jpg',
+    category: 'drone',
+    title: 'Road from Drone',
+  },
+  {
+    id: '7',
+    url: 'portrait1.jpg',
+    category: 'portrait',
+    title: 'Portrait 1',
+  },
+  {
+    id: '8',
+    url: 'wedding1.jpg',
+    category: 'wedding',
+    title: 'Wedding 1',
+  },
+  {
+    id: '9',
+    url: 'hills.jpg',
+    category: 'landscape',
+    title: 'Hills',
+  },
+  {
+    id: '10',
+    url: 'baby1.jpg',
+    category: 'baby',
+    title: 'Baby 1',
+  },
+  {
+    id: '11',
+    url: 'portrait2.jpg',
+    category: 'portrait',
+    title: 'Portrait 2',
+  },
+  {
+    id: '12',
+    url: 'lake.jpg',
+    category: 'landscape',
+    title: 'Lake',
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('done');
   document.querySelector('.toggle').addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('close');
     document.querySelector('.links').classList.toggle('links-hidden');
+  });
+
+  const filters = document.querySelector('#filters');
+  const gallery = document.querySelector('#gallery');
+  const modal = document.querySelector('#modal');
+  const closeModal = document.querySelector('#close-modal');
+
+  const filterImages = (filter) => {
+    const images = document.querySelectorAll('.gallery-image');
+    images.forEach((image) => {
+      if (image.dataset.category !== filter && filter !== 'all') {
+        image.classList.add('image-hidden');
+        image.classList.remove('image-show');
+      } else {
+        image.classList.remove('image-hidden');
+        image.classList.add('image-show');
+      }
+    });
+  };
+
+  const showModal = (e) => {
+    const imageId = e.target.dataset.id;
+    const modalImgs = document.querySelectorAll('.modal-image');
+    modalImgs.forEach((img) => {
+      if (img.dataset.id !== imageId) {
+        img.classList.add('modal-image-hidden');
+        img.classList.remove('modal-image-show');
+      } else {
+        img.classList.remove('modal-image-hidden');
+        img.classList.add('modal-image-show');
+      }
+    });
+    modal.style.display = 'block';
+  };
+
+  categories.forEach((element) => {
+    const domElement = document.createElement('p');
+    domElement.innerHTML = element.name;
+    domElement.classList.add('filter-element');
+    domElement.id = element.filter;
+    domElement.addEventListener('click', (e) => filterImages(e.target.id));
+    filters.appendChild(domElement);
+  });
+
+  images.forEach((element) => {
+    const image = document.createElement('img');
+    image.src = `images/${element.url}`;
+    image.classList = 'gallery-image';
+    image.dataset.category = element.category;
+    image.dataset.id = element.id;
+    image.addEventListener('click', (e) => showModal(e));
+    gallery.appendChild(image);
+
+    const modalImage = document.createElement('img');
+    modalImage.src = `images/${element.url}`;
+    modalImage.classList = 'modal-image modal-image-hidden';
+    modalImage.dataset.id = element.id;
+    modal.appendChild(modalImage);
+  });
+
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
   });
 });
